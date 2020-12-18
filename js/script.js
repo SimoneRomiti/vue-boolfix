4ad18412cccd3e2edfee11273258a313c6038dc9
Simone Romiti <sr1992ei@gmail.com>
2020-12-18 14:15:17 +0100

    Attivazione/disattivazione info con CSS

--- js/script.js
+++ js/script.js
@@ -2,15 +2,10 @@ var app= new Vue(
   {
     el: "#container",
     data: {
-      mouse: false,
+
     },
     methods: {
-      turnTrue: function(){
-        this.mouse = true;
-      },
-      turnFalse: function(){
-        this.mouse = false;
-      }
+      
     }
   }
 )
