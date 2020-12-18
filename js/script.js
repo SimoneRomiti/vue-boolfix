bd50acd2bf8a062127535e8c2e80eb468ffbfad4
Simone Romiti <sr1992ei@gmail.com>
2020-12-18 16:30:17 +0100

    Ricerca film con chiamata axios

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-var app= new Vue(
+var app = new Vue(
   {
     el: "#container",
     data: {
-
+      films: [],
+      searchedString: "",
     },
+
     methods: {
-      
+      searchFilm: function(){
+        var self = this;
+        axios
+        .get('https://api.themoviedb.org/3/search/movie',
+        {
+          params: {
+            api_key: "00d4d16d41869351335359c44741a330",
+            language: "it-IT",
+            query: self.searchedString
+          }
+        })
+        .then(function(response){
+          self.films = response.data.results;
+          console.log(self.films);
+        })
+
+      }
     }
   }
-)
+);
