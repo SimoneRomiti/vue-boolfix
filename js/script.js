9e1117503f268dd84b9e9f054892755e5003e9f2
Simone Romiti <sr1992ei@gmail.com>
2020-12-21 22:06:15 +0100

    console log del cast

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var app = new Vue(
     data: {
       films: [],
       all: [],
+      actors: [],
+      filmActors: [],
       searchedString: "",
       nullColor: 'grey',
       notAvailableImage: "img/image-not-available.png",
@@ -44,28 +46,77 @@ var app = new Vue(
 
           Promise.all([getOne, getTwo])
           .then(function(response){
-            self.all = [];
+
             self.films = [];
 
-            self.all = response[0].data.results.concat(response[1].data.results);
+            self.all =
+            response[0].data.results            .concat(response[1].data.results);
 
             self.all.sort(function(a, b){
               return b.popularity - a.popularity;
             })
 
-            console.log("all", self.all);
+
             var i = 0;
             while(i < 20 && self.all[i] != null){
               self.films.push(self.all[i]);
               i++;
             }
 
+
             console.log("films", self.films);
+
+            const creditsInitialPath = 'https://api.themoviedb.org/3/movie/';
+            const creditsFinalPath = '/credits';
+            const creditsSeriePath = 'https://api.themoviedb.org/3/tv/'
 
             for(var i = 0; i < self.films.length; i++){
               self.vote[i] = Math.ceil(self.films[i].vote_average);
             }
-            console.log("vote", self.vote);
+
+            for(var i = 0; i < self.films.length; i++){
+              if(self.films[i].title != null){
+
+                axios
+                .get(creditsInitialPath + self.films[i].id + creditsFinalPath,
+                {
+                  params: {
+                    api_key: "00d4d16d41869351335359c44741a330",
+                    language: "it-IT",
+                  }
+                })
+
+
+                .then((response) => {
+                  for(var k = 0; k < 5; k++){
+                    if(response.data.cast[k] != null){
+                      console.log(response.data.cast[k].name);
+                    }
+                  }
+                })
+                
+              } else {
+                axios
+                .get(creditsSeriePath + self.films[i].id + creditsFinalPath,
+                {
+                  params: {
+                    api_key: "00d4d16d41869351335359c44741a330",
+                    language: "it-IT",
+                  }
+                })
+
+
+                .then((response) => {
+                  for(var k = 0; k < 5; k++){
+                    if(response.data.cast[k] != null){
+                      console.log(response.data.cast[k].name);
+                    }
+                  }
+                })
+              }
+
+            }
+
           })
         }
       },
